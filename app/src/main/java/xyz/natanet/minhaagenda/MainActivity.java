6190a9757e4d7d6a3fa65977462fbbc6d6d866e7
natanasilva <nataniel.natanaguiar@gmail.com>
2022-11-13 16:17:21 -0300

    Implementando novas funções

--- app/src/main/java/xyz/natanet/minhaagenda/MainActivity.java
+++ app/src/main/java/xyz/natanet/minhaagenda/MainActivity.java
@@ -2,10 +2,9 @@ package xyz.natanet.minhaagenda;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
-import android.widget.Toast;
 import androidx.annotation.Nullable;
-
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -13,17 +12,14 @@ import java.util.List;
 
 public class MainActivity extends Activity {
 
-    protected void onCreate(@Nullable Bundle SavedInstanceState) {
-        super.onCreate(SavedInstanceState);
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        List<String> alunos = new ArrayList<>(Arrays.asList("Alex", "Fran", "Jose"));
-        TextView primeiroAluno = findViewById(R.id.nomeAluno1);
-        TextView segundoAluno = findViewById(R.id.nomeAluno2);
-        TextView terceiroAluno = findViewById(R.id.nomeAluno3);
-
-        primeiroAluno.setText(alunos.get(0));
-        segundoAluno.setText(alunos.get(1));
-        terceiroAluno.setText(alunos.get(2));
-
+        List<String> alunos = new ArrayList<>(
+                Arrays.asList("Alex", "Fran", "Jose", "Maria", "Ana"));
+        ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
+        listaDeAlunos.setAdapter(new ArrayAdapter<>(
+                this, android.R.layout.simple_list_item_1, alunos));
     }
 }
